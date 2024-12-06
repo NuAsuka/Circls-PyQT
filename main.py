13fd14d72e4795543c59288c9ba1d8e14d5f2d8b
Rochar22 <m.zamorskiy7@gmail.com>
2024-12-06 17:56:23 +0300

    v1

--- main.py
+++ main.py
@@ -14,16 +14,10 @@ class MainWindow(QWidget):
         self.pushButton1.clicked.connect(self.draw_circle)
         self.circles = []
 
-    def random_color_hsv(self):
-        h = random.randint(0, 360)
-        s = random.randint(0, 255)
-        v = random.randint(128, 255)
-        return QColor.fromHsv(h, s, v)
-
     def paintEvent(self, event):
         painter = QPainter(self)
-        for x, y, diameter, color in self.circles:
-            pen = QPen(color, 2)
+        for x, y, diameter in self.circles:
+            pen = QPen(QColor('Yellow'), 2)
             painter.setPen(pen)
             painter.drawEllipse(x, y, diameter, diameter)
 
@@ -33,9 +27,8 @@ class MainWindow(QWidget):
         diameter = random.randint(10, 50)
         x = random.randint(0, width - diameter)
         y = random.randint(0, height - diameter)
-        color = self.random_color_hsv()
-        self.circles.append((x, y, diameter, color))
-        self.update() #update
+        self.circles.append((x, y, diameter))
+        self.update()  # update
 
 
 if __name__ == "__main__":
